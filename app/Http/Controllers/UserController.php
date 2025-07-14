956766235513358c3934b4499096fdb9859431e6
carlkiptoo <carlosiamcarl@gmail.com>
2025-07-14 17:12:39 +0300

    Creating a user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,4 +17,27 @@ class UserController extends Controller
 
         return response()->json($users, Response::HTTP_OK);
     }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'role' => 'in:admin,user',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role ?? 'user',
+        ]);
+
+        return response()->json(['message' => 'User created successfully', 'user' => $user], Response::HTTP_CREATED);
+    }
 }
