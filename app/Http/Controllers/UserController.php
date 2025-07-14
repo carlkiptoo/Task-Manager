1c07471ea3f838f699b2d806d7ac5dba50bd73fb
carlkiptoo <carlosiamcarl@gmail.com>
2025-07-14 17:15:25 +0300

    Updating user info

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,4 +40,27 @@ class UserController extends Controller
 
         return response()->json(['message' => 'User created successfully', 'user' => $user], Response::HTTP_CREATED);
     }
+
+    public function update(Request $request, $id) 
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'sometimes|required|string|max:255',
+            'email' => 'sometimes|required|email|max:255|unique:users,email,' . $id,
+            'role' => 'sometimes|in:admin,user',
+        ]);
+        
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $user->update($request->only(['name', 'email', 'role']));
+
+        return response()->json(['message' => 'User updated successfully', 'user' => $user], Response::HTTP_OK);
+    }
 }
